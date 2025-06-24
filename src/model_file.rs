a4dda58b37c3d6185e0d0ed34846dfffc4b2951a
Tomás Vallotton <tvallotton@uc.cl>
2025-06-24 16:59:03 -0400

    add render functions

--- src/model_file.rs
+++ src/model_file.rs
@@ -9,12 +9,24 @@ pub struct ModelFile {
 }
 
 impl ModelFile {
-    pub fn imports(&self) -> impl Iterator<Item = &str> {
-        self.models.iter().flat_map(|model| model.imports())
+    pub fn imports(&self) -> BTreeSet<&str> {
+        self.models
+            .iter()
+            .flat_map(|model| model.imports())
+            .chain(Some("import dataclasses"))
+            .collect::<BTreeSet<&str>>()
     }
 
-    pub fn render_file(&self) -> String {
-        todo!()
+    pub fn add_model(&mut self, model: Model) {
+        self.models.push(model);
+        self.models
+            .sort_by(|m1, m2| m2.python_type.annotation.cmp(&m1.python_type.annotation));
+    }
+
+    pub fn render(&self) -> String {
+        let imports = self.imports();
+
+        return minijinja::render!(include_str!("../templates/model_file.py.jinja2"), imports=> imports,models=> &self.models);
     }
 }
 
@@ -24,14 +36,52 @@ fn model_file_imports() {
     let model_file = ModelFile {
         models: vec![model],
     };
-    assert!(model_file.imports().any(|import| import == "import uuid"));
+    assert!(model_file.imports().contains("import uuid"));
 }
 
 #[test]
 fn model_file_render() {
+    let expected = r#"import dataclasses
+import uuid
+
+
+@dataclass.dataclass
+class User:
+    email: str
+    id: uuid.UUID
+"#;
+
     let model = Model::from_table(&mock::user_table(), &Options::default());
     let model_file = ModelFile {
         models: vec![model],
     };
-    let contents = model_file.render_file();
+    let contents = model_file.render();
+    assert_eq!(contents, expected);
+}
+
+#[test]
+fn model_file_render_two_tables() {
+    let expected = r#"import dataclasses
+import uuid
+
+
+@dataclass.dataclass
+class User:
+    email: str
+    id: uuid.UUID
+
+@dataclass.dataclass
+class UserReview:
+    id: uuid.UUID
+    url: str
+"#;
+
+    let model1 = Model::from_table(&mock::user_table(), &Options::default());
+    let model2 = Model::from_table(&&mock::user_review_table(), &Options::default());
+    let mut model_file = ModelFile::default();
+    model_file.add_model(model1);
+    model_file.add_model(model2);
+    let contents = model_file.render();
+    println!("{contents}");
+    assert_eq!(contents, expected);
 }
--- src/model_file_generator.rs
+++ src/model_file_generator.rs
@@ -1,26 +1,38 @@
 #[cfg(test)]
 use crate::mock;
-use crate::model_file::ModelFile;
+use crate::{model_file::ModelFile, utils::to_snake_case};
 use std::collections::BTreeMap;
 
 pub struct ModelFileGenerator {
+    pub package: String,
     pub default_schema: String,
     pub model_files: BTreeMap<String, ModelFile>,
 }
 
 impl ModelFileGenerator {
-    pub fn new(default_schema: &str) -> Self {
+    pub fn new(package: &str, default_schema: &str) -> Self {
         ModelFileGenerator {
-            default_schema: default_schema.into(),
+            package: package.into(),
+            default_schema: to_snake_case(default_schema),
             model_files: Default::default(),
         }
+    }
+
+    pub fn init_file_contents(&self) -> String {
+        let model_file = self.model_files.get(&self.default_schema);
+        minijinja::render!(include_str!("../templates/model_init.py.jinja2"), package => self.package, default_schema => self.default_schema, models => model_file.unwrap().models )
     }
 }
 
 #[test]
 fn model_file_generator_generate() {
-    let mut generator = ModelFileGenerator::new("public");
+    let mut generator = ModelFileGenerator::new("app", "public");
     let mut public = ModelFile::default();
     public.models.push(mock::user_model());
     generator.model_files.insert("public".into(), public);
+
+    let expected = r#"from app.models.public import (
+    User,
+)"#;
+    assert_eq!(generator.init_file_contents(), expected);
 }
